02ea0aff8bb5df8c9ca290f166769d70846f3ddf
Abhishek Rauthan <abhishekrauthan733@gmail.com>
2023-05-23 11:06:16 +0530

    git commit -m "02"

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const messageRoutes = require("./routes/messages");
 const app = express();
 const socket = require("socket.io");
 require("dotenv").config();
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
@@ -25,8 +26,8 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const server = app.listen(process.env.PORT, () =>
-  console.log(`Server started on ${process.env.PORT}`)
+const server = app.listen(PORT, () =>
+  console.log(`Server started on ${PORT}`)
 );
 const io = socket(server, {
   cors: {
